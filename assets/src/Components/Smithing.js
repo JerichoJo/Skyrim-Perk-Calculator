66312fd3e805987250a4a17112e016fd61504004
Jordan Lang <jordanlang410@gmail.com>
2022-01-26 19:27:36 -0500

    update Smithing tree

--- assets/src/Components/Smithing.js
+++ assets/src/Components/Smithing.js
@@ -10,6 +10,10 @@ const SmithingTree = () => {
 
     const [count, setCount] = useState(0);
 
+    const circleRadius = '8';
+    const circleStrokeWidth = '4';
+    const lineStrokeWidth = '5'
+
     const incrementCount = () => {
         setCount(count + 1);
         console.log(count);
@@ -49,27 +53,29 @@ const SmithingTree = () => {
     return (
         <View>
             <Svg height="900" width="500">
-                <Line
-                    x1="150"
-                    y1="400"
-                    x2="100"
-                    y2="600"
+                <Circle
+                    cx="200"
+                    cy="700"
+                    r={circleRadius}
                     stroke={button1Color}
-                    id="line1"
-                    strokeWidth="5"
+                    strokeWidth={circleStrokeWidth}
+                    fill="white"
                     onPress={() => {
                         checkIfPressed1(button1Color == 'blue' ? 'red' : 'blue');
                         incrementCount();
                     }}
                 />
+
+
                 <Circle
-                    cx="100"
-                    cy="600"
-                    r="8"
-                    fill={button1Color}
+                    cx="80"
+                    cy="550"
+                    r={circleRadius}
+                    stroke={button2Color}
+                    strokeWidth={circleStrokeWidth}
+                    fill="white"
                     onPress={() => {
-                        checkIfPressed1(button1Color == 'blue' ? 'red' : 'blue');
-                        incrementCount();
+                        checkIfPressed2(button2Color == 'blue' ? 'red' : 'blue');
                     }}
                 />
                 <Line
@@ -78,18 +84,76 @@ const SmithingTree = () => {
                     x2="150"
                     y2="400"
                     stroke={button2Color}
-                    strokeWidth="5"
+                    strokeWidth={lineStrokeWidth}
                     onPress={() => {
                         checkIfPressed2(button2Color == 'blue' ? 'red' : 'blue');
                     }}
                 />
+
                 <Circle
-                    cx="150"
-                    cy="400"
-                    r="8"
-                    fill={button2Color}
+                    cx="110"
+                    cy="470"
+                    r={circleRadius}
+                    stroke={button3Color}
+                    strokeWidth={circleStrokeWidth}
+                    fill="white"
                     onPress={() => {
-                        checkIfPressed2(button2Color == 'blue' ? 'red' : 'blue');
+                        checkIfPressed3(button3Color == 'blue' ? 'red' : 'blue');
+                    }}
+                />
+                <Circle
+                    cx="180"
+                    cy="430"
+                    r={circleRadius}
+                    stroke={button3Color}
+                    strokeWidth={circleStrokeWidth}
+                    fill="white"
+                    onPress={() => {
+                        checkIfPressed3(button3Color == 'blue' ? 'red' : 'blue');
+                    }}
+                />
+                <Circle
+                    cx="250"
+                    cy="430"
+                    r={circleRadius}
+                    stroke={button3Color}
+                    strokeWidth={circleStrokeWidth}
+                    fill="white"
+                    onPress={() => {
+                        checkIfPressed3(button3Color == 'blue' ? 'red' : 'blue');
+                    }}
+                />
+                <Circle
+                    cx="320"
+                    cy="450"
+                    r={circleRadius}
+                    stroke={button3Color}
+                    strokeWidth={circleStrokeWidth}
+                    fill="white"
+                    onPress={() => {
+                        checkIfPressed3(button3Color == 'blue' ? 'red' : 'blue');
+                    }}
+                />
+                <Circle
+                    cx="400"
+                    cy="500"
+                    r={circleRadius}
+                    stroke={button3Color}
+                    strokeWidth={circleStrokeWidth}
+                    fill="white"
+                    onPress={() => {
+                        checkIfPressed3(button3Color == 'blue' ? 'red' : 'blue');
+                    }}
+                />
+                <Circle
+                    cx="400"
+                    cy="500"
+                    r={circleRadius}
+                    stroke={button3Color}
+                    strokeWidth={circleStrokeWidth}
+                    fill="white"
+                    onPress={() => {
+                        checkIfPressed3(button3Color == 'blue' ? 'red' : 'blue');
                     }}
                 />
                 <Line
@@ -98,20 +162,12 @@ const SmithingTree = () => {
                     x2="150"
                     y2="300"
                     stroke={button3Color}
-                    strokeWidth="5"
+                    strokeWidth={lineStrokeWidth}
                     onPress={() => {
                         checkIfPressed3(button3Color == 'blue' ? 'red' : 'blue');
                     }}
                 />
-                <Circle
-                    cx="150"
-                    cy="300"
-                    r="8"
-                    fill={button3Color}
-                    onPress={() => {
-                        checkIfPressed3(button3Color == 'blue' ? 'red' : 'blue');
-                    }}
-                />
+
             </Svg>
         </View>
     );
