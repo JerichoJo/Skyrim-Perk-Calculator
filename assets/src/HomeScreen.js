7ee296ec95fae6603ab6c5189bd931bac780ee0a
Jordan Lang <jordanlang410@gmail.com>
2022-01-26 20:10:12 -0500

    Fix scrolling and add other perks

--- assets/src/HomeScreen.js
+++ assets/src/HomeScreen.js
@@ -16,16 +16,29 @@ const HomeScreen = ({ navigation }) => {
     const [RequiredLevel, setRequiredLevel] = useState(0);
 
     const [Perks, setPerks] = useState([
-        { name: "Smithing", key: '1', image: '../images/HealthBG.jpg' },
-        { name: "Heavy Armor", key: '2', image: '../images/HealthBG.jpg' },
-        { name: "Block", key: '3', image: '../images/HealthBG.jpg' },
-        { name: "Two-Handed", key: '4', image: '../images/HealthBG.jpg' },
-        { name: "One-Handed", key: '5', image: '../images/HealthBG.jpg' },
-        { name: "Archery", key: '6', image: '../images/HealthBG.jpg' }
+        { name: "Alchemy", key: '1', image: '../images/HealthBG.jpg', tree: <SmithingTree /> },
+        { name: "Illusion", key: '2', image: '../images/HealthBG.jpg' },
+        { name: "Conjuration", key: '3', image: '../images/HealthBG.jpg' },
+        { name: "Destruction", key: '4', image: '../images/HealthBG.jpg' },
+        { name: "Restoration", key: '5', image: '../images/HealthBG.jpg' },
+        { name: "Alteration", key: '6', image: '../images/HealthBG.jpg' },
+        { name: "Enchanting", key: '7', image: '../images/HealthBG.jpg' },
+        { name: "Smithing", key: '8', image: '../images/HealthBG.jpg', tree: <SmithingTree /> },
+        { name: "Heavy Armor", key: '9', image: '../images/HealthBG.jpg' },
+        { name: "Block", key: '10', image: '../images/HealthBG.jpg' },
+        { name: "Two-Handed", key: '11', image: '../images/HealthBG.jpg' },
+        { name: "One-Handed", key: '12', image: '../images/HealthBG.jpg' },
+        { name: "Archery", key: '13', image: '../images/HealthBG.jpg' },
+        { name: "Light Armor", key: '14', image: '../images/HealthBG.jpg' },
+        { name: "Sneak", key: '15', image: '../images/HealthBG.jpg' },
+        { name: "Lockpicking", key: '16', image: '../images/HealthBG.jpg' },
+        { name: "Pickpocket", key: '17', image: '../images/HealthBG.jpg' },
+        { name: "Speech", key: '18', image: '../images/HealthBG.jpg' },
     ])
 
     return (
         <>
+
             <ImageBackground
 
                 style={styles.Container}
@@ -41,6 +54,7 @@ const HomeScreen = ({ navigation }) => {
                     data={Perks}
                     renderItem={({ item }) => (
                         <>
+
                             <Text style={styles.item}> {item.name}</Text>
 
                             <View style={{
@@ -51,37 +65,33 @@ const HomeScreen = ({ navigation }) => {
                                 bottom: 0,
                                 left: 0,
                                 top: 0,
-                                right: 0
+                                right: 0,
+                                width: "90%",
+
                             }}>
-                                <TouchableOpacity>
-                                    <FontAwesome name="circle-thin" size={14} color="white" />
-                                </TouchableOpacity>
+                                {item.tree}
 
-                            </View></>
+                            </View>
 
+                        </>
 
                     )
                     }
 
                 />
                 <View
-                    style={{
-                        position: 'absolute',
-                        top: 500,
-                        left: 0,
-                        right: 0,
-                        bottom: 0,
-                        justifyContent: 'center',
-                        alignItems: 'center'
-                    }}
+                    style={styles.bottomText}
+
                 >
                     <Text style={styles.HomeScreenText}>Active Perks: {ActivePerks}/21</Text>
                     <Text style={styles.HomeScreenText}>Required Skill: {RequiredSkill} </Text>
                     <Text style={styles.HomeScreenText}>All Active Perks: {AllActivePerks}</Text>
                     <Text style={styles.HomeScreenText}>Required level: {RequiredLevel} </Text>
-                    <SmithingTree />
+
                 </View>
+
             </ImageBackground>
+
 
         </>
     );
@@ -91,16 +101,28 @@ const HomeScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
     Container: {
         flex: 1,
-        width: null,
-        height: null,
+        width: "100%",
+        height: "100%",
         justifyContent: 'center',
         backgroundColor: '#000000',
         flexDirection: 'column',
-        alignItems: 'flex-start'
+        alignItems: 'flex-start',
 
     },
     HomeScreenText: {
         color: 'white',
+
+
+    },
+
+    bottomText: {
+        position: 'absolute',
+        top: 500,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
 
     },
     item: {
@@ -111,7 +133,8 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         marginTop: 22
-    }
+
+    },
 
 });
 
