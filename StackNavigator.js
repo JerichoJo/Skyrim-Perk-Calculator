9ec78344b12ced7827f73c365021c1a9dc5b894d
Jordan Lang <jordanlang410@gmail.com>
2022-02-11 16:54:15 -0500

    take out un-needed notes

--- StackNavigator.js
+++ StackNavigator.js
@@ -9,45 +9,16 @@ import Modal from "./assets/src/Components/Modals/Modal";
 //import { createDrawerNavigator } from "@react-navigation/drawer";
 
 
-// const Stack = createNativeStackNavigator();
-
-// const StackNavigator = () => (
-
-//     <Stack.Navigator screenOptions={{ headerShown: false }}>
-
-//         <Stack.Screen name="HomeScreen" component={HomeScreen} />
-
-
-//     </Stack.Navigator>
-// );
-
-const ModalNav = createBottomTabNavigator();
+const Stack = createBottomTabNavigator();
 const ModalNavScreen = () => (
-    <ModalNav.Navigator screenOptions={{ headerShown: false }} tabBarOptions={{ activeBackgroundColor: "grey" }} >
+    <Stack.Navigator screenOptions={{ headerShown: false }} tabBarOptions={{ activeBackgroundColor: "grey" }} >
 
-        <ModalNav.Screen name="Homescreen" component={HomeScreen} options={{ tabBarIcon: () => <AntDesign name="home" size={40} /> }} />
+        <Stack.Screen name="Homescreen" component={HomeScreen} options={{ tabBarIcon: () => <AntDesign name="home" size={40} /> }} />
 
-        <ModalNav.Screen name="Modal" component={Modal} options={{ tabBarIcon: () => <AntDesign name="bars" size={40} /> }} />
+        <Stack.Screen name="Modal" component={Modal} options={{ tabBarIcon: () => <AntDesign name="bars" size={40} /> }} />
 
-    </ModalNav.Navigator>
+    </Stack.Navigator>
 );
-
-// const AppTabs = createBottomTabNavigator();
-// const AppTabScreen = () => (
-//     <AppTabs.Navigator screenOptions={{ headerShown: false }} tabBarOptions={{ activeBackgroundColor: "grey" }} >
-
-
-//         <AppTabs.Screen name="Nav" component={StackNavigator} options={{ tabBarIcon: () => <AntDesign name="bars" size={40} /> }} />
-
-//     </AppTabs.Navigator>
-// );
-
-// const AppDrawer = createDrawerNavigator();
-// const AppDrawerScreen = () => (
-//     <AppDrawer.Navigator>
-//         <AppDrawer.Screen name="tabs" component={AppTabScreen} />
-//     </AppDrawer.Navigator>
-// )
 
 export default () => (
     <NavigationContainer>
