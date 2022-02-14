79efaea4ae24833e7a56d3967c0a5370435e6c1a
Jordan Lang <jordanlang410@gmail.com>
2022-02-14 09:22:07 -0500

    Move Stack from App.js

---
+++ StackNavigator.js
@@ -0,0 +1,90 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { Text, Dimensions, LogBox, View } from 'react-native';
+import { useState } from 'react';
+LogBox.ignoreLogs(["Require cycle:"]);
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import HomeScreen from "./assets/src/HomeScreen";
+import BasicSmithingModal from "./assets/src/Components/Modals/BasicSmithingModal";
+import ArcaneSmithingModal from "./assets/src/Components/Modals/ArcaneSmithingModal";
+import { DrawerContent } from './assets/src/Components/Modals/DrawerNav';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+//import { createDrawerNavigator } from "@react-navigation/drawer";
+// cardOverlayEnabled: true,
+// contentStyle: { backgroundColor: "#40404040" }
+
+const Drawer = createDrawerNavigator();
+const ModalNav = createNativeStackNavigator();
+
+const { width } = Dimensions.get('window');
+
+export const AllActivePerkss = React.createContext(0);
+
+function Modal() {
+    return (
+
+        <ModalNav.Navigator screenOptions={{ headerShown: false }}  >
+
+            <ModalNav.Screen name="Homescreen" component={HomeScreen} />
+
+            <ModalNav.Group screenOptions={{ presentation: "transparentModal" }} >
+
+                <ModalNav.Screen name="BasicSmithingModal" component={BasicSmithingModal} />
+                <ModalNav.Screen name="ArcaneSmithingModal" component={ArcaneSmithingModal} />
+
+            </ModalNav.Group>
+
+        </ModalNav.Navigator>
+    );
+}
+
+function Drawers() {
+    const [AllActivePerks, SetAllActivePerks] = useState(0);
+    return (
+        <AllActivePerkss.Provider value={[AllActivePerks, SetAllActivePerks]}>
+            <Drawer.Navigator
+                drawerContent={props => <DrawerContent {...props} />}
+                initialRouteName='HomeScreen'
+                screenOptions={{
+                    headerShown: true,
+                    headerTransparent: true,
+                    headerTintColor: 'white',
+                    headerTitleAlign: 'center',
+                    headerTitle: 'All Active Perks: ' + AllActivePerks,
+                    headerRight: () => {
+                        <Text
+                            style={{
+                                color: 'white',
+                                zIndex: 8
+                            }}
+                        >{AllActivePerks}</Text>
+                    },
+                    drawerStyle: {
+                        borderColor: 'white',
+                        borderWidth: 1,
+                        backgroundColor: 'slateblue',
+                        width: 225,
+                        shadowColor: 'black',
+                        borderRadius: 5,
+
+                    }
+                }}
+            >
+
+                <Drawer.Screen name='BasicSmithingModal ' component={Modal} />
+
+            </Drawer.Navigator>
+        </AllActivePerkss.Provider>
+
+    );
+}
+
+export default () => (
+
+    <NavigationContainer>
+        <Drawers />
+    </NavigationContainer>
+
+);
