12c6192da3a3aa14b2e8a7b4107d9fd051fbc69d
Jordan Lang <jordanlang410@gmail.com>
2022-02-14 21:02:18 -0500

    add index.js for modals

---
+++ assets/src/Components/Modals/index.js
@@ -0,0 +1,9 @@
+import BasicSmithingModal from "./SmithingTreeModals/BasicSmithingModal";
+import ArcaneSmithingModal from "./SmithingTreeModals/ArcaneSmithingModal";
+const modals = {
+    BasicSmithingModal,
+    ArcaneSmithingModal,
+
+}
+
+export default modals;
--- assets/src/Components/index.js
+++ assets/src/Components/index.js
@@ -14,4 +14,5 @@ const tree = {
     EnchantingTree,
     SneakTree,
 }
+
 export default tree;
