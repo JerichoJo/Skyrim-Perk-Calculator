19f58a072f2170de073e147cc246cefc072dbcad
abstew <annastewart1@me.com>
2022-02-24 12:13:10 -0500

    Pushing Block and Destruction tree modals

---
+++ assets/src/Components/Modals/BlockModals/ElementalProtectionModal.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+
+const ElementalProtectionModal = ({ navigation }) => {
+
+    return (
+        <View
+            style={styles.Container}
+        >
+            <Pressable
+                style={[
+                    StyleSheet.absoluteFill,
+                ]}
+                onPress={navigation.goBack}
+            />
+
+            <View
+                style={styles.TextBox}
+                onPress={navigation.goBack}>
+                <Text style={styles.Title}>Elemental Protection{"\n"}</Text>
+
+                <Text style={styles.Text}>Blocking with a shield reduces incoming fire, frost, and shock damage by 50%</Text>
+
+            </View>
+
+        </View>
+    );
+}
+export default ElementalProtectionModal;
+
+const styles = StyleSheet.create({
+    Container: {
+        flex: 1,
+        backgroundColor: 'transparent',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '5%'
+    },
+    TextBox: {
+        backgroundColor: 'black',
+        padding: '7%',
+        borderRadius: 25
+    },
+    Title: {
+        fontSize: 22,
+        color: 'white',
+        textAlign: 'center',
+        textDecorationLine: 'underline',
+    },
+    Text: {
+        fontSize: 18,
+        color: 'white',
+        textAlign: 'center',
+    },
+});
