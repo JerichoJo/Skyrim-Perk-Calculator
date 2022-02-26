756ccb438bf60ed479cfcd082dd838798290f5a9
abstew <annastewart1@me.com>
2022-02-25 20:09:50 -0500

    Pushing two modals and text

---
+++ assets/src/Components/Modals/OneHandedModals/ParalyzingStrikeModal.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+
+const ParalyzingStrikeModal = ({ navigation }) => {
+
+    return (
+        <View
+            style={styles.Container}
+        >
+            <Pressable
+                style={[
+                    StyleSheet.absoluteFill,
+                ]}
+                onPress={navigation.goBack}
+            />
+
+            <View
+                style={styles.TextBox}
+                onPress={navigation.goBack}>
+                <Text style={styles.Title}>Paralyzing Strike{"\n"}</Text>
+
+                <Text style={styles.Text}>Backwards power attack has a 25% chance to paralyze target</Text>
+
+            </View>
+
+        </View>
+    );
+}
+export default ParalyzingStrikeModal;
+
+const styles = StyleSheet.create({
+    Container: {
+        flex: 1,
+        backgroundColor: 'transparent',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '5%'
+    },
+    TextBox: {
+        backgroundColor: 'black',
+        padding: '7%',
+        borderRadius: 25
+    },
+    Title: {
+        fontSize: 22,
+        color: 'white',
+        textAlign: 'center',
+        textDecorationLine: 'underline',
+    },
+    Text: {
+        fontSize: 18,
+        color: 'white',
+        textAlign: 'center',
+    },
+});
